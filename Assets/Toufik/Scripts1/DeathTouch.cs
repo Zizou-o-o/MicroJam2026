78f08e2c1ddfad1b02608f12c000f9b68c4f98c0
Zizou-o-o <zizourabhi76@gmail.com>
2026-03-31 15:27:21 +0200

    saved the scenes in the build

--- Assets/Toufik/Scripts1/DeathTouch.cs
+++ Assets/Toufik/Scripts1/DeathTouch.cs
@@ -23,15 +23,4 @@ public class DeathTouch : MonoBehaviour
 
 
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
